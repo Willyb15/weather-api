8070776afd00c8962df5e0cc23010505e2504c76
Willyb15 <william.steen.bryant@gmail.com>
2016-03-25 17:50:54 -0400

    Added icons

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,13 @@ $(document).ready(function(){
 
 	var apiKey = "d7406b223ea5ae42946d0344fa0567fb";
 	var weatherURL = "http://api.openweathermap.org/data/2.5/weather?q=Atlanta&units=imperial&APPID=" + apiKey;
+	
 	var weatherArray = [];
+
 	
 	$.getJSON(weatherURL, function(weatherData){
 		console.log(weatherData);
+			
 		// we want temperature for starters. The temperature in their JSON is at:
 		// weatherData.main
 		var currTemp = weatherData.main.temp;
@@ -17,30 +20,35 @@ $(document).ready(function(){
 		var currPerc = 0;
 		
 		var newHTML = ' ';
-
-
+		var iconId = weatherData.weather[0].icon;
+		var iconDes = weatherData.weather[0].description;
+		var weatherIcon = "<img src=http://openweathermap.org/img/w/"+ iconId +".png>"
 		var cityDefault = "Atlanta";
 		var weatherHumid = "Humidity: " + weatherData.main.humidity;
 		var weatherMax = "Max Temp: " + weatherData.main.temp_max;
 		var weatherMin = "Min Temp " + weatherData.main.temp_min;
 		var weatherPress = "Pressure: " + weatherData.main.pressure;
+		var windSpeed = "Wind Speed: " + weatherData.wind.speed;
+		
+		console.log(iconId);
+		console.log(weatherIcon);
 
-		weatherArray.push(weatherHumid, weatherMax, weatherMin, weatherPress);
+
+		weatherArray.push(weatherHumid, weatherMax, weatherMin, weatherPress, windSpeed);
 		for(var i=0; i<weatherArray.length; i++){
 					
-			newHTML += '<div class="col-sm-3">';
+			newHTML += '<div class="col-sm-4 weather-info">';
 			newHTML += '<p>' + weatherArray[i] + '</p>';
 			newHTML += '</div>';	
 		}
-		newHTML += '<div col-sm-3 text-center weather-info' + '<p>' + cityDefault + '<p>' + '</div>'; 
+		newHTML += '<div class="col-sm-4 text-center weather-info"' + '<p>' + weatherIcon + iconDes + '<p>' + '</div>'; 
+		newHTML += '<div class="row">'
+		newHTML += '<div class="col-sm-12 text-center weather-info city"' + '<p>' + cityDefault + '<p>' + '</div>'; 
+		newHTML += '</div>'
 		
 		$('#weather-grid').html(newHTML);
 
 
-
-
-		//Set up our circle and styling
-		// set up our color based on temp (colder = bluer, hotter = redder)
 		var shadeColor;
 		if(currTemp < 32){
 			shadeColor = '#D4F0FF';
@@ -63,7 +71,7 @@ $(document).ready(function(){
 			context.fill();
 
 			context.lineWidth = 10;
-			context.strokeStyle = "#ff0000";
+			context.strokeStyle = shadeColor;
 			context.beginPath();
 			context.arc(155, 75, 70, Math.PI*1.5, (Math.PI*2 * current) + Math.PI*1.5);
 			context.stroke();
@@ -109,17 +117,23 @@ $(document).ready(function(){
 			var weatherMax = "Max Temp: " + cityData.main.temp_max;
 			var weatherMin = "Min Temp " + cityData.main.temp_min;
 			var weatherPress = "Pressure: " + cityData.main.pressure;
-			var windSpeed = cityData.wind.speed;
-			console.log(windSpeed);
+			var windSpeed = "Wind Speed: " + cityData.wind.speed;
+			var iconId = cityData.weather[0].icon;
+			var iconDes = cityData.weather[0].description;
+			var weatherIcon = "<img src=http://openweathermap.org/img/w/"+ iconId +".png>"
 
-			tempArray.push(weatherHumid, weatherMax, weatherMin, weatherPress);
+			tempArray.push(weatherHumid, weatherMax, weatherMin, weatherPress, windSpeed);
 			for(var i=0; i<tempArray.length; i++){
 						
-				newHTML += '<div class="col-sm-3 weather-info">';
+				newHTML += '<div class="col-sm-4 weather-info">';
 				newHTML += '<p>' + tempArray[i] + '</p>';
 				newHTML += '</div>';
 			}
-			newHTML += '<div col-sm-3 text-center weather-info' + '<p>' + cityName +'<p>' + '</div>'; 	
+		newHTML += '<div class="col-sm-4 text-center weather-info"' + '<p>' + weatherIcon + iconDes + '<p>' + '</div>'; 
+		newHTML += '<div class="row">'
+		newHTML += '<div class="col-sm-12 text-center weather-info city"' + '<p>' + cityName + '<p>' + '</div>'; 
+		newHTML += '</div>'	
+		newHTML +=	'<div class="col-sm-12 text-center">' + "<p>Current Temp: <p>" +'</div>';
 		
 		$('#weather-grid').html(newHTML);
 
